61b620a1892a5e151520c611f095986fa7172006
Hugo Le Moal <huglemoal@gmail.com>
2022-07-04 16:40:02 +0200

    clean useless files

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -42,14 +42,6 @@ class TaskController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'app_task_show', methods: ['GET'])]
-    public function show(Task $task): Response
-    {
-        return $this->render('task/show.html.twig', [
-            'task' => $task,
-        ]);
-    }
-
     #[Route('/{id}/edit', name: 'app_task_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Task $task, TaskRepository $taskRepository): Response
     {
