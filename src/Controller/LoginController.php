f6b4e29d2f25fd40d1a00bc04197d36ab797d521
Hugo Le Moal <huglemoal@gmail.com>
2022-07-04 16:34:18 +0200

    login / logout

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -24,4 +24,10 @@ class LoginController extends AbstractController
             'error' => $error,
         ]);
     }
+
+    #[Route('/logout', name: 'app_logout')]
+    public function logout()
+    {
+        throw new \Exception('This should never be reached!');
+    }
 }
